1df7b02b21e6000cd34af68bbe917ceedc41565f
T-H-10 <t0534198344@gmail.com>
2025-05-07 18:36:48 +0300

    show canvas without press

--- src/components/creatingCard/editCanvas/EditableCanvas.tsx
+++ src/components/creatingCard/editCanvas/EditableCanvas.tsx
@@ -20,92 +20,81 @@ const EditableCanvas = ({ cardData }: { cardData: GreetingCard }) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
     const fabricRef = useRef<fabric.Canvas | null>(null);
     const [loading, setLoading] = useState<boolean>(false);
-    // const { currentCard } = useContext(CurrentCardContext);
     const currentUserId = useContext(UserContext).user.id;
     const { selectedTemplate } = useSelector(selectTemplates);
-    console.log(cardData);
 
     const loadCanvasFromJSON = (json: string, fabricRef: React.RefObject<fabric.Canvas | null>) => {
-
         if (!fabricRef.current) return;
         fabricRef.current.setHeight(500); // גובה הקנבס
         fabricRef.current.setWidth(500); // רוחב הקנבס
-        // המרה של JSON לאובייקט JS
-        const parsedJSON = JSON.parse(json);
-        // if (fabricRef.current) { 
-        //     fabricRef.current.clear(); // ננקה את הקנבס לפני טעינת ה-JSON
-        // }
-        // יצירת קנבס מתוך ה-JSON
-        fabricRef.current.loadFromJSON(parsedJSON);
-        console.log(parsedJSON.objects);
-
-        // נוסיף את כל הטקסטים מה-JSON לקנבס
-
-        parsedJSON.objects.forEach((obj: any) => {
-            if (obj.type === "Textbox") {
-                const { left, top, width, height, fontSize, fontWeight, fontFamily, fill, text, textAlign, angle } = obj;
-
-                const textObject = new fabric.Textbox(text, {
-                    left,
-                    top,
-                    width,
-                    height,
-                    fontSize,
-                    fontWeight,
-                    fontFamily,
-                    fill,
-                    textAlign,
-                    angle,
-                    originX: 'left',
-                    originY: 'top',
-                });
-                if (fabricRef.current) {
-                    // הוספת הטקסט לקנבס
-                    fabricRef.current.add(textObject);
-                }
-            }
-        });
-        if (fabricRef.current) {
-            fabricRef.current.renderAll(); // רענן את הקנבס
+        let parsedJSON;
+        try{
+            parsedJSON = JSON.parse(json);
+        } catch(e){
+            console.log("שגיאה בטעינת הJSON", e);
+            return;
         }
+        // const parsedJSON = JSON.parse(json);
+        fabricRef.current.loadFromJSON(parsedJSON, () => {
+            parsedJSON.objects.forEach((obj: any) => {
+                if (obj.type === "Textbox") {
+                    const { left, top, width, height, fontSize, fontWeight, fontFamily, fill, text, textAlign, angle } = obj;
+
+                    const textObject = new fabric.Textbox(text, {
+                        left,
+                        top,
+                        width,
+                        height,
+                        fontSize,
+                        fontWeight,
+                        fontFamily,
+                        fill,
+                        textAlign,
+                        angle,
+                        originX: 'left',
+                        originY: 'top',
+                    });
+                    if (fabricRef.current) {
+                        fabricRef.current.add(textObject);
+                    }
+                }
+            });
+
+            // if (fabricRef.current) {
+                fabricRef.current?.renderAll(); // רענן את הקנבס
+            // }
+        });
     };
 
     useEffect(() => {
-        console.log(selectedTemplate);
-
+const setupCanvas=async()=>{
         const element = canvasRef.current;
         if (!element) return;
 
-        const canvas = new fabric.Canvas(element, { preserveObjectStacking: true });
-        fabricRef.current = canvas;
-
+        if (!fabricRef.current) {
+            const canvas = new fabric.Canvas(element, { preserveObjectStacking: true });
+            fabricRef.current = canvas;
+        }
         // אם יש תבנית, נוודא שטוענים את הרקע והתבנית כראוי
         if (cardData && cardData.templateID) {
             const { canvasStyle, templateID } = cardData;
             // אם התבנית לא טעונה, נבקש לטעון אותה
-            if (selectedTemplate.templateID !== templateID || !selectedTemplate) {
-                console.log("טעינה מחדש של התבנית...");
-
-                dispatch(fetchTemplateById(templateID));
+            if (!selectedTemplate || selectedTemplate.templateID !== templateID) {
+                // console.log("טעינה מחדש של התבנית...");
+                await dispatch(fetchTemplateById(templateID));
             }
+            const imageURL = selectedTemplate.imageURL || DEFAULT_IMAGE;
             // אם יש סגנון קנבס, נטען אותו. אחרת נטען את התמונה מהתבנית או דיפולטיבית
             if (canvasStyle) {
                 loadCanvasFromJSON(canvasStyle, fabricRef);
             } else {
-                const imageURL = selectedTemplate.imageURL || DEFAULT_IMAGE;
                 setCanvasBackground(imageURL, setLoading, fabricRef);
-                //אין עיצוב מותאם
-                // if (selectedTemplate.imageURL) {
-                //     setCanvasBackground(selectedTemplate.imageURL, setLoading, fabricRef);
-                // } else {
-                //     setCanvasBackground(DEFAULT_IMAGE, setLoading, fabricRef);
-                // }
             }
         }
+    };
+    setupCanvas();
         return () => {
-            if (fabricRef.current) {
-                fabricRef.current.dispose();
-            }
+            fabricRef.current?.dispose();
             fabricRef.current = null;
         };
     }, [cardData, dispatch, selectedTemplate]);
@@ -113,37 +102,36 @@ const EditableCanvas = ({ cardData }: { cardData: GreetingCard }) => {
     const saveDesign = () => {
         if (fabricRef.current) {
             try {
-            const json = JSON.stringify(fabricRef.current.toJSON());
-            // try{
-            // dispatch(fetchGreetingCardById(cardData.cardID));
-            // }catch(e: any){
-            //     if(e.status===404){
-            //         console.log(e);
-            //     }
-            // }
-            // console.log(selectedCard);
-            // if (selectedCard) 
-            if(cardData.cardID!=-1)
-                {
+                const json = JSON.stringify(fabricRef.current.toJSON());
+                // try{
+                // dispatch(fetchGreetingCardById(cardData.cardID));
+                // }catch(e: any){
+                //     if(e.status===404){
+                //         console.log(e);
+                //     }
+                // }
+                // console.log(selectedCard);
+                // if (selectedCard) 
+                if (cardData.cardID != -1) {
                     dispatch(updateGreetingCard({
                         id: cardData.cardID,
                         updatedCard: {
                             ...cardData,
                             canvasStyle: json
                         }
-                    }))
+                    }));
                     Swal.fire({ icon: 'success', title: 'העיצוב עודכן בהצלחה!' });
-            } else{
-                dispatch(addGreetingCard({
-                    userID: currentUserId,
-                    textID: cardData.textID,
-                    categoryID: cardData.categoryID,
-                    templateID: cardData.templateID,
-                    canvasStyle: json,
-                }));
-                Swal.fire({ icon: 'success', title: 'העיצוב נשמר בהצלחה!' });
-            }
-            
+                } else {
+                    dispatch(addGreetingCard({
+                        userID: currentUserId,
+                        textID: cardData.textID,
+                        categoryID: cardData.categoryID,
+                        templateID: cardData.templateID,
+                        canvasStyle: json,
+                    }));
+                    Swal.fire({ icon: 'success', title: 'העיצוב נשמר בהצלחה!' });
+                }
+
             } catch (error) {
                 Swal.fire({
                     icon: 'error',

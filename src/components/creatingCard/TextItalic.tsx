1739b1e57a826faba9899cf2d8a0c6588b39f0ed
T-H-10 <t0534198344@gmail.com>
2025-05-21 19:37:31 +0300

    delete not used code

--- src/components/creatingCard/TextItalic.tsx
+++ src/components/creatingCard/TextItalic.tsx
@@ -50,36 +50,3 @@ const TextItalic: React.FC<TextItalicProps> = ({ canvas }) => {
 
 export default TextItalic
 
-
-// import { useState } from 'react';
-// import FormatItalicRoundedIcon from '@mui/icons-material/FormatItalicRounded';
-// import MyOptionToStyleButton from '../style/MyOptionToStyleButton';
-// import * as fabric from 'fabric';
-// // interface CustomFabricText extends fabric.IText {
-// //     fontStyle?: "italic" | "normal"; // הוספת המאפיין המותאם אישית
-// // }
-
-// const TextItalic = ({ canvas }: { canvas: fabric.Canvas }) => {
-//     const [isItalic, setIsItalic] = useState<boolean>(false);
-
-//     const toggleItalic = () => {
-//         const activeObject = canvas?.getActiveObject() as fabric.IText | null;
-//         if (activeObject && activeObject.type === 'textbox') {
-//             const newFontStyle = isItalic ? 'italic' : 'normal';
-//             activeObject.set('fontStyle', newFontStyle);
-//             setIsItalic(!isItalic);
-//             canvas?.renderAll();
-//         }
-//     };
-
-//     return (
-//         <>
-//         <MyOptionToStyleButton onClick={toggleItalic}
-//             isActive={!isItalic}>
-//             <FormatItalicRoundedIcon />
-//         </MyOptionToStyleButton>
-//         </>
-//     );
-// };
-
-// export default TextItalic;

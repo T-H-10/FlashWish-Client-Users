be13f7e681c539d134a1776931da815ef62015d7
T-H-10 <t0534198344@gmail.com>
2025-05-28 11:50:53 +0300

    category-list with fit-content width

--- src/components/categories/CategoriesList.tsx
+++ src/components/categories/CategoriesList.tsx
@@ -1,54 +1,54 @@
-import { createContext, useContext, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { appDispatch } from '../../Store/Store';
-import { Category } from '../../types/CategoryTypes';
-import AddCategory from './AddCategory ';
-import MyButton from '../style/MyButton';
-import LoadingIndicator from '../LoadingIndicator';
-import { selectCategories } from '../../Store/categoriesStore/CategoriesSlice';
-import { addCategory, fetchCategories } from '../../Store/categoriesStore/CategoriesApi';
-import { IsLogin } from '../../App';
+// import { createContext, useContext, useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { appDispatch } from '../../Store/Store';
+// import { Category } from '../../types/CategoryTypes';
+// import AddCategory from './AddCategory ';
+// import MyButton from '../style/MyButton';
+// import LoadingIndicator from '../LoadingIndicator';
+// import { selectCategories } from '../../Store/categoriesStore/CategoriesSlice';
+// import { addCategory, fetchCategories } from '../../Store/categoriesStore/CategoriesApi';
+// import { IsLogin } from '../../App';
 
 
-export const CategoriesListContext = createContext<Category[]>([]);
+// export const CategoriesListContext = createContext<Category[]>([]);
 
-const CategoriesList = ({ onCategorySelect }: { onCategorySelect: Function }) => {
-    const dispatch = useDispatch<appDispatch>();
-    const { categoriesList, loading } = useSelector(selectCategories);
-    const [isLogin] = useContext(IsLogin);
-    useEffect(() => {
-        dispatch(fetchCategories());
-    }, [dispatch]);
+// const CategoriesList = ({ onCategorySelect }: { onCategorySelect: Function }) => {
+//     const dispatch = useDispatch<appDispatch>();
+//     const { categoriesList, loading } = useSelector(selectCategories);
+//     const [isLogin] = useContext(IsLogin);
+//     useEffect(() => {
+//         dispatch(fetchCategories());
+//     }, [dispatch]);
 
-    const handleAddCategory = (newCategory: { categoryName: string }) => {
-        dispatch(addCategory(newCategory));
-    };
+//     const handleAddCategory = (newCategory: { categoryName: string }) => {
+//         dispatch(addCategory(newCategory));
+//     };
 
-    return (
-        <>
-            {categoriesList.length === 0 && !loading && (
-                <div>
-                    לא נמצאו קטגוריות, אנא הוסף קטגוריה חדשה
-                </div>
-            )}
-            {categoriesList.length > 0 && (
-                <div>
-                    <h3>בחר קטגוריה:</h3>
-                    {categoriesList.map((category: Category) => (
-                        <span key={category.categoryID}>
-                            <MyButton onClick={() => onCategorySelect(category.categoryID)} content={category.categoryName} />
-                        </span>
-                    ))}
-                </div>)
-            }
-            {isLogin &&
-                <AddCategory existingCategories={categoriesList.length>0? categoriesList?.map(category => category.categoryName): []} onAddCategory={handleAddCategory} />
-            }
-            {loading &&
-                <LoadingIndicator content='טוען קטגוריות' />
-            }
-        </>
-    );
-};
+//     return (
+//         <>
+//             {categoriesList.length === 0 && !loading && (
+//                 <div>
+//                     לא נמצאו קטגוריות, אנא הוסף קטגוריה חדשה
+//                 </div>
+//             )}
+//             {categoriesList.length > 0 && (
+//                 <div>
+//                     <h3>בחר קטגוריה:</h3>
+//                     {categoriesList.map((category: Category) => (
+//                         <span key={category.categoryID}>
+//                             <MyButton onClick={() => onCategorySelect(category.categoryID)} content={category.categoryName} />
+//                         </span>
+//                     ))}
+//                 </div>)
+//             }
+//             {isLogin &&
+//                 <AddCategory existingCategories={categoriesList.length>0? categoriesList?.map(category => category.categoryName): []} onAddCategory={handleAddCategory} />
+//             }
+//             {loading &&
+//                 <LoadingIndicator content='טוען קטגוריות' />
+//             }
+//         </>
+//     );
+// };
 
-export default CategoriesList;
+// export default CategoriesList;
